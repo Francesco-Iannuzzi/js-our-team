e574ec449894930a4f9f2994d92a8347f36da904
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-03-22 14:20:33 +0100

    add array / object / for on array + log

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,13 +15,13 @@ Scott Estrada	Developer	scott-estrada-developer.jpg
 Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
 
 MILESTONE 0:
-Creare l’array di oggetti con le informazioni fornite.
+Creare l’array di oggetti con le informazioni fornite. ✓
 
 MILESTONE 1:
-Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto. ✓
 
 MILESTONE 2:
-Stampare le stesse informazioni su DOM sottoforma di stringhe
+Stampare le stesse informazioni su DOM sottoforma di stringhe. 
 
 BONUS 1:
 Trasformare la stringa foto in una immagine effettiva
@@ -35,3 +35,58 @@ Consigli del giorno:
 - E ricordiamoci che console.log() è nostro amico!
 
 */
+
+/*
+
+TOOLS:
+- const / let
+- array
+- object
+- for / while
+- querySelector / getElementById
+- innerHTML / createElement / insertAdjacentHTML
+
+*/
+
+//creare un array che contenga i vari membri e definiamo l'array in una costante
+const memberTeam = [
+    //definire all'interno dell'array i vari membri tramite object e definire le lore caratteristiche
+    {
+        name: 'Wayne Barnett',
+        job: 'Founder & CEO',
+        img: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        job: 'Chief Editor',
+        img: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        job: 'Office Manager',
+        img: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        job: 'Social Media Manager',
+        img: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        job: 'Developer',
+        img: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        img: 'barbara-ramos-graphic-designer.jpg'
+    }
+] 
+
+//creare un ciclo per ciclare nell'array dei membri 
+for (let i = 0; i < memberTeam.length; i++) {
+    //salvare i vari membri in una costante e loggarli in console
+    const member = memberTeam[i];
+    console.log(member.name, member.job, member.img);
+    
+}
